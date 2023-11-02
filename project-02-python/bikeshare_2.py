c039ae14b3241542e673280fc2b67751e257c7a3
Cong Dinh <congdinh2012@hotmail.com>
2023-11-03 03:09:47 +0700

    Create doc comment for time_stats method to

--- project-02-python/bikeshare_2.py
+++ project-02-python/bikeshare_2.py
@@ -137,7 +137,16 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """
+    Displays statistics on the most frequent times of travel.
+
+    Args:
+        df - Pandas DataFrame containing city data filtered by month and day
+    Returns:
+        None
+    """
+
+    
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
